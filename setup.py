8c40315e4d03128b7e3009f1d3194ed49dff635c
Brian Rotich <23168146+brotich@users.noreply.github.com>
2020-04-05 22:45:28 +0300

    add setting for the setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+setuptools.setup(
+    name="graphql_error_logger",
+    version="0.0.1",
+    author="Brian Rotich",
+    author_email="opensource@brianrotich.com",
+    description="Log expection from the GraphQL python server",
+    url="https://github.com/brotich/graphql_error_logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'flask_graphql>=2.0.0'
+    ]
+)
