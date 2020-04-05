9522b8d808a5a325c031ef804730595c244cafa7
Brian Rotich <23168146+brotich@users.noreply.github.com>
2020-04-05 22:32:42 +0300

    add docstring for the functions

--- graphql_logger/__init__.py
+++ graphql_logger/__init__.py
@@ -4,14 +4,30 @@ from flask_graphql import GraphQLView
 from graphql_server import default_format_error
 
 
-def init_logger(logger=None):
+def init_logger(logger: logging = None):
+    """[summary]
+
+    Keyword Arguments:
+        logger {logging} -- [description] (default: {None})
+
+    Returns:
+        [type] -- [description]
+    """
     if not logger:
         logger = logging.getLogger("graphql-error-logger")
         logger.setLevel(logging.ERROR)
 
     @staticmethod
     def test_logging_update(error):
-        logger.error('error in resolvers', exc_info=error.original_error)
+        """[summary]
+
+        Arguments:
+            error {[type]} -- [description]
+
+        Returns:
+            [type] -- [description]
+        """
+        logger.error("error in resolvers", exc_info=error.original_error)
         return default_format_error(error)
 
     GraphQLView.format_error = test_logging_update
